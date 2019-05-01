e45f471b6bee33d749ccbc8593325e5d67a88283
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-01 20:50:01 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,38 @@
+  def instantiate_new_array
+     array = []
+     return array
+    end
+  
+  def array_with_two_elements
+      array_2 = ["a", "b"]
+      return array_2
+    end
+  
+  def first_element(array_first)
+      array_first[0]
+   end
+
+  def third_element(array_third)
+      array_third[2]
+   end
+  
+  def last_element(array_last)
+      array_last[-1]
+    end
+  
+  def first_element_with_array_methods(array_first_method)
+     array_first_method.first
+    end
+  
+  def last_element_with_array_methods(array_last_method)
+     array_last_method.last
+    end
+    
+  def length_of_array(array_count)
+     array_count.count
+    end
+    
+    
+    
+    
+  
